da45eed4ef93e5c8eeb5d2669d1618c0e9670493
khalil chehata <kalil.chehata@gmail.com>
2025-08-20 03:25:42 +0100

    Update

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,3 +1,4 @@
+import { Home } from "lucide-react";
 import Navbar from "../Component/Website/Header/Navbar";
 import AdsenseAd from "../Dadsense/Dadsense";
 import PopunderAd from "../PopunderAd/PopunderAd";
@@ -6,7 +7,7 @@ const Layouts = ({ children }) => {
     return (
         <>
             <Navbar />
-            <main>qsdd</main>
+            <main><Home></Home></main>
 
         </>
     );
