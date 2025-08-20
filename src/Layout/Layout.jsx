9b40cbf4eb73f49209b25722cecb1ff4bfd4548e
khalil chehata <kalil.chehata@gmail.com>
2025-08-20 03:37:14 +0100

    Update

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -3,7 +3,25 @@ import Home from "../Component/Website/Home/Home";
 import AdsenseAd from "../Dadsense/Dadsense";
 import PopunderAd from "../PopunderAd/PopunderAd";
 
-const Layouts = ({ children, NativeAd }) => {
+const Layouts = ({ children }) => {
+
+    // Native Ad يظهر في كل صفحة
+    const NativeAd = () => {
+        useEffect(() => {
+            const container = document.getElementById("native-ad");
+            if (container) {
+                const script = document.createElement("script");
+                script.src = "//pl27448508.profitableratecpm.com/c39b3bd3eab4b0b5a5910cf7fc622ee2/invoke.js";
+                script.async = true;
+                script.dataset.cfasync = "false";
+                container.appendChild(script);
+                return () => container.removeChild(script);
+            }
+        }, []);
+
+        return <div id="native-ad" style={{ margin: "0 0" }}></div>;
+    };
+
     return (
         <>
             <PopunderAd />
