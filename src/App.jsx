e924ec8d62fd5a610c7b8236a9772d5b044f87f1
khalil chehata <kalil.chehata@gmail.com>
2025-08-20 14:52:24 +0100

    Update index.html with organized ads

--- src/App.jsx
+++ src/App.jsx
@@ -168,22 +168,9 @@ function App() {
 
 
 
-  // Native Ad يظهر في كل صفحة
-  const NativeAd = () => {
-    useEffect(() => {
-      const container = document.getElementById("native-ad");
-      if (container) {
-        const script = document.createElement("script");
-        script.src = "//pl27448508.profitableratecpm.com/c39b3bd3eab4b0b5a5910cf7fc622ee2/invoke.js";
-        script.async = true;
-        script.dataset.cfasync = "false";
-        container.appendChild(script);
-        return () => container.removeChild(script);
-      }
-    }, []);
 
-    return <div id="native-ad" style={{ margin: "0 0" }}></div>;
-  };
+
+
   return (
 
     <>
