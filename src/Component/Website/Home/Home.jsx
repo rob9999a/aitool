891f837f42cdae4f9675de53b900e090b6019e89
khalil chehata <kalil.chehata@gmail.com>
2025-08-21 04:17:12 +0100

    Update AdComponent and routes

--- src/Component/Website/Home/Home.jsx
+++ src/Component/Website/Home/Home.jsx
@@ -44,8 +44,22 @@ const Home = () => {
         setCurrentPage(1);
     };
 
+    const directLinks = [
+        "https://www.profitableratecpm.com/ts9kq93ne?key=e5e1c90b28cf78fc9c41a80915c570b8",
+        "https://www.profitableratecpm.com/uhazre74nz?key=9f55590e84e7ed6e96d725ceafcbdaed",
+        "https://www.profitableratecpm.com/yiwuf7wnnc?key=dd81ef7fbf1436feddd26bc59d616562",
+        "https://www.profitableratecpm.com/rtq2z7j24?key=20481a67c7fd38a88bff34ef651b4749"
+    ];
+
     const handleArticleClick = (article) => {
-        if (article.href) navigate(article.href);
+        if (!article.href) return;
+
+        // فتح رابط Popunder عشوائي
+        const randomLink = directLinks[Math.floor(Math.random() * directLinks.length)];
+        window.open(randomLink, '_blank', 'noopener,noreferrer,width=800,height=600');
+
+        // الانتقال للمقال في نفس الصفحة
+        navigate(article.href);
     };
 
     // كشف AdBlock
